9615572c1b00420fb5d9315923ec08cbfd0a0646
Logan Joecks <ljoecks@gmail.com>
2017-04-25 13:34:24 -0400

    add preferences to children

--- src/pages/children/children.ts
+++ src/pages/children/children.ts
@@ -13,46 +13,85 @@ import moment from 'moment';
   Ionic pages and navigation.
 */
 @Component({
-    selector: 'page-children',
-    templateUrl: 'children.html'
+  selector: 'page-children',
+  templateUrl: 'children.html'
 })
 export class ChildrenPage {
-    children: FirebaseListObservable < any[] >
+  afChildren: FirebaseListObservable < any[] >
+    children: any[]
+  childPreferences: any[]
 
-        constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, af: AngularFire, private _auth: AuthService) {
-            let uid = _auth.getUid()
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public af: AngularFire, private _auth: AuthService) {
+    let uid = _auth.getUid()
 
-            this.children = af.database.list('/children/' + uid);
-        }
+    this.afChildren = af.database.list('/children/' + uid);
+    this.childPreferences = [];
+  }
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad ChildrenPage');
+  ionViewDidLoad() {
+    this.afChildren
+      .subscribe(data => {
+        this.children = data
+
+        this.children.forEach((child) => {
+          this.childPreferences[child.$key] = this.af.database.object('/preferences/' + child.$key)
+            // .subscribe(prefData => {
+            //   this.childPreferences[child.$key] = prefData
+            // })
+        })
+      })
+  }
+
+  addChild() {
+    let modal = this.modalCtrl.create(AddChildModalPage);
+    modal.present();
+
+    modal.onDidDismiss(data => {
+      if (data) {
+        this.afChildren.push(data)
+      }
+    });
+  }
+
+  getAge(dob) {
+    let yearsOld = moment().diff(dob, 'years');
+
+    if (yearsOld < 2) {
+      return 'Age: ' + moment().diff(dob, 'months') + ' months'
+    } else {
+      return 'Age: ' + yearsOld;
+    }
+  }
+
+  goToChild(child) {
+    console.log(child.$key)
+    this.navCtrl.push(ChildPage, child)
+  }
+
+  getPreferenceState(child, entity) {
+    let preference = this.childPreferences[child.$key];
+
+    if (!preference) {
+      return '';
     }
 
-    addChild() {
-        let modal = this.modalCtrl.create(AddChildModalPage);
-        modal.present();
-
-        modal.onDidDismiss(data => {
-            if (data) {
-                this.children.push(data)
-            }
-        });
+    if (!preference[entity] || !preference[entity].state) {
+      return 'Off'
+    } else {
+      return 'On';
     }
+  }
 
-    getAge(dob) {
-        let yearsOld = moment().diff(dob, 'years');
-
-        if (yearsOld < 2) {
-            return moment().diff(dob, 'months') + ' months'
-        } else {
-            return yearsOld + ' years';
-        }
-    }
-
-    goToChild(child) {
-        console.log(child.$key)
-        this.navCtrl.push(ChildPage, child)
-    }
+  toggleExpand(childToToggle) {
+    this.children.forEach(child => {
+      if (childToToggle.$key === child.$key) {
+        setTimeout(() => {
+          childToToggle.expanded = !childToToggle.expanded
+        }, 200)
+      } else {
+        child.expanded = false;
+      }
+    })
+  }
 
 }
